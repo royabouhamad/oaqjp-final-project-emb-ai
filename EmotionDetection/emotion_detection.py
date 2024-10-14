2c81708aa083a825733961790f4b2023369fed6e
royabouhamad <roy.d.abouhamad@gmail.com>
2024-10-14 17:42:05 +0100

    Create emotion detector and package it

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=myobj, headers=header)
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = {'emotion': '', 'score': 0}
+
+    for emotion, score in emotions.items():
+        if score > dominant_emotion['score']:
+            dominant_emotion = {'emotion': emotion, 'score': score}
+
+    emotions['dominant_emotion'] = dominant_emotion['emotion']
+    return emotions
